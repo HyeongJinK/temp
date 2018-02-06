d2f4d334b4e7cf4e293693db087936855c998a88
estgames <wkzkfmxk23@gmail.com>
2018-02-06 14:51:44 +0900

    Initial commit

---
+++ Example/pod_gitlab_test/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  pod_gitlab_test
+//
+//  Created by wkzkfmxk23@gmail.com on 02/06/2018.
+//  Copyright (c) 2018 wkzkfmxk23@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
