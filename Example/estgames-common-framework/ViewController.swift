a99e2eab3b1d0f67b3e09ae72ae3c224292dc460
estgames <wkzkfmxk23@gmail.com>
2018-02-07 10:59:50 +0900

    Initial commit

---
+++ Example/estgames-common-framework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  estgames-common-framework
+//
+//  Created by wkzkfmxk23@gmail.com on 02/07/2018.
+//  Copyright (c) 2018 wkzkfmxk23@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
