05b85221efa67e3416fd05dbf107d047bc46de50
estgames <wkzkfmxk23@gmail.com>
2018-03-26 18:00:44 +0900

    temp

--- common/ResultDataJson.swift
+++ common/ResultDataJson.swift
@@ -10,12 +10,22 @@ import Foundation
 
 class ResultDataJson {
     var nation: String = ""
-    var process: Process
-    
+    var process: [Process]
+    var events : Event
 }
 
 class Process {
     
 }
 
-class Event
+class Event {
+    var begin: String?
+    var end: String?
+    var banners: [Banner]
+}
+
+class Banner {
+    var name: String = ""
+    var resource : String = ""
+    var writer : String?
+}
