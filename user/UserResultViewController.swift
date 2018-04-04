30897bc428e6429c3bcb8a60c63c953bab2ae3b7
estgames <wkzkfmxk23@gmail.com>
2018-04-04 18:03:03 +0900

    유저

--- user/UserResultViewController.swift
+++ user/UserResultViewController.swift
@@ -10,6 +10,8 @@ import Foundation
 class UserResultViewController: UIViewController {
     var userResultTitle: UILabel! //알림
     var closeButton: UIButton!//닫기
+    var lineView: UIView!
+    var subLabe: UILabel!
     //서브 타이틀
     //글
     //유아ㅗㅅ=
