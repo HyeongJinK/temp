8390460254adbbba155037d9c52bef896a82baa1
jupic-web <juphich@gmail.com>
2018-08-01 11:05:52 +0900

    기능개선  - 세션 리포지토리에 토큰정보를 별도의 필드로 중복 저장

--- estgames_framework/app/src/main/java/com/estgames/estgames_framework/common/UserService.kt
+++ estgames_framework/app/src/main/java/com/estgames/estgames_framework/common/UserService.kt
@@ -20,7 +20,6 @@ import java.lang.Exception
 import java.util.concurrent.Callable
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import kotlin.math.sign
 
 /**
  * Created by mp on 2018. 5. 2..
